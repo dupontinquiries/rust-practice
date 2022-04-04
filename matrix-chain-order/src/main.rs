de4285214778d0867a4deb98c6b9439d614fc62e
Kessler D <43118393+dupontinquiries@users.noreply.github.com>
2022-04-04 12:29:33 -0400

    Update main.rs

--- matrix-chain-order/src/main.rs
+++ matrix-chain-order/src/main.rs
@@ -18,32 +18,93 @@ fn mtx_mlt_ord_min_ops_recrsive(dims: Vec<(u64, u64)>) -> ((u64, u64), u64) {
         return (final_dims, cost);
     }
     // else we will take the min of the subcombinations
-    // return the min of all the combinations
     let mut val = 0 as u64;
-    for i in 1..dims.len() {
+    for i in 1..dims.len() - 1 {
+        println!(" i = {0}", i as u64);
         let sp1: Vec<(u64, u64)> = (&dims[..i]).to_vec();
         let sp2: Vec<(u64, u64)> = (&dims[i..]).to_vec();
         let p1 = mtx_mlt_ord_min_ops_recrsive(sp1); // dims and cost of p1
         let p2 = mtx_mlt_ord_min_ops_recrsive(sp2); // dims and cost of p2
         let total_cost = p1.1 + p2.1 + (p1.0.0 * p2.0.1 * p1.0.1); // cost of the two subproblemms plus the cost of multiplying them together
-        // let p3 = ;// cost of p1*p2
-        // println!("splitting @ {0}", (i) as u64);
-        // println!(" {0} + {1}", p1, p2);
-        if val == 0 || val > total_cost {
+        println!("  splitting @ {} -> {:?} | {:?}", i as u64, p1.0, p2.0);
+        println!("  {0} + {1} + {2} = {3}", p1.1, p2.1, p1.0.0 * p2.0.1 * p1.0.1, total_cost);
+        if i == 1 || val > total_cost {
             val = total_cost;
         }
-        // TODO: implement the cost of multiplying the subproblems together
     }
+    // return the min of all the combinations
     return ((0,0),val);
 }
 
+// use std::cmp;
+
+fn mtx_mlt_ord_min_ops_dp(dims: Vec<(u64,u64)>) -> u64 {
+    let mut t: Vec<u64> = Vec::with_capacity(dims.len()*dims.len()/2);
+    // t[0 + dims.len()*0] = 0;
+    for i in 0..dims.len() {
+        t[i*dims.len()+i] = 0; // table[i][i] = 0
+        for j in i+1..dims.len() {
+            for k in i..j {
+                let num = t[(i-1)*dims.len()+j] + t[i*dims.len()+j-1] + (0);
+                if k == i || t[i*dims.len()+j] > num {
+                    t[i*dims.len()+j] = num;    
+                }
+            }
+        }
+    }
+    return 0;
+}
+
+fn mco(dims: Vec<u64>) -> u64 {
+    let n = dims.len();
+    let mut t: Vec<u64> = Vec::with_capacity(dims.len()*dims.len()/2);
+    for i in 1..n {
+        t[i*n+i] = 0;
+    }
+    for L in 2..n {
+        for i in 1..n-L+1 {
+            let j = i+L-1;
+            t[i*n+j] = u64::MAX;
+            for k in i..j-1 {
+                let q = t[i*n+k] + t[(k+1)*n+j] + dims[i-1]*dims[k]*dims[j];
+                if q < t[i*n+j] {
+                    t[i*n+j] = q;
+                }
+            }
+        }
+    }
+    return t[n+n];
+}
+
+// fn convert(v: Vec<(u64, u64)>) -> Vec<u64> {
+//     let mut v2: Vec<u64> = Vec::with_capacity(v.len());
+//     for i in 0..v.len() {
+//         v2[i] = v[i].0;//.append(v[i].0);
+//     }
+//     return v2;
+// }
+
 fn main() {
     println!("running tests...");
-    print!(" recursive algorithm tests..."); // must be passed so we can use it to check the accuracy of the dp algorithm
-    assert_eq!(mtx_mlt_ord_min_ops_recrsive(vec![(1,2), (2,1)]).1, 2);
-    assert_eq!(mtx_mlt_ord_min_ops_recrsive(vec![(4,3), (3,2), (2,1)]).1, 18);
-    println!(" done");
+            // println!(" recursive algorithm tests..."); // must be passed so we can use it to check the accuracy of the dp algorithm
+            // // assert_eq!(mtx_mlt_ord_min_ops_recrsive(vec![(1,2), (2,1)]).1, 2);
+            // // assert_eq!(mtx_mlt_ord_min_ops_recrsive(vec![(4,3), (3,2), (2,1)]).1, 18);
+            // // assert_eq!(mtx_mlt_ord_min_ops_recrsive(vec![(4,10), (10,3), (3,12), (12,20), (20, 7)]).1, 1344);
+            // println!(" done");
     print!(" dynamic programming algorithm tests...");
+    // let problems = vec![
+    //     (vec![(1,2), (2,1)], 2),
+    //     (vec![(4,3), (3,2), (2,1)], 18),
+    //     (vec![(4,10), (10,3), (3,12), (12,20), (20, 7)], 1344)
+    // ];
+    let problems = vec![
+        (vec![1, 2, 1], 2),
+        (vec![4, 3, 2, 1], 18),
+        (vec![4, 3, 12, 20, 7], 1344)
+    ];
+    for i in 0..problems.len() {
+        assert_eq!(mco(problems[i].0.to_vec()), problems[i].1);
+    }
     println!(" done");
     println!("passed all tests");
 }
