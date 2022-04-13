e41a5ce8b755ebd33a1e938c04ef7de8906a93d6
Kessler D <43118393+dupontinquiries@users.noreply.github.com>
2022-04-13 19:34:44 -0400

    Update main.rs

--- dp-palindrome/src/main.rs
+++ dp-palindrome/src/main.rs
@@ -4,9 +4,9 @@
 
 fn main() {
     println!("running tests...");
-    assert_eq!(longest_palindrome("ACGTGTCAAAATCG".to_string()), 4);
     assert_eq!(longest_palindrome("ATATTA".to_string()), 4);
     assert_eq!(longest_palindrome("ATATTAT".to_string()), 6);
+    assert_eq!(longest_palindrome("ACGTGTCAAAATCG".to_string()), 4);
     println!("passed all tests");
 }
 
@@ -16,19 +16,34 @@ fn longest_palindrome(s: String) -> i64 {
     let v: Vec<char> = s.chars().collect::<Vec<_>>();
     let n = v.len();
     let mut t = vec![1 as i64; n];
-    for i in 0..n {
-        // build subproblems
-        let c1 = v[i];
+    for i in n-2..=0 {
         for j in i..n {
-            let c2 = v[j];
-            println!("{}\n{}", fv_char(&v), fv_i64(&t));
-            if c1 == c2 {
-            } else {
-                t[i] += (j - i) as i64;
-                break;
-            }
+            // if 
         }
     }
+// // for i in 0..n {
+//     // build subproblems
+//     for j in 0..n {
+//         println!("{}\n{}", fv_char(&v), fv_i64(&t));
+//         let c1 = v[j];
+//         let mut k = j + t[j] as usize;
+//         while k < n - 1 {
+//             k += t[k] as usize;
+//             let c2 = v[k];
+//             if c1 == c2 && (k - j) as i64 > t[j] {
+//                 t[j] = (k - j) as i64;
+//             }
+//         }
+//         // for k in j..n {
+//         //     let c2 = v[k];
+//         //     if (k - i) as i64 > t[i] && c1 == c2 {
+//         //         t[i] = (k - i) as i64;
+//         //         // this is almost it... just need to account for the fact that I am not checking if the middle is a palindrome
+//         //         // TODO: need to check each corresponding element and jump ahead that number
+//         //     }
+//         // }
+//     }
+// }
     // return max from list
     // code adapted from https://stackoverflow.com/questions/58669865/how-to-get-the-minimum-value-within-a-vector-in-rust
     let max_val = t.iter().max();
