bf084169fea79efb675bc85c5f4e5498b8dbcaee
Kessler D <43118393+dupontinquiries@users.noreply.github.com>
2022-04-13 20:04:49 -0400

    Update main.rs

--- dp-palindrome/src/main.rs
+++ dp-palindrome/src/main.rs
@@ -27,33 +27,7 @@ fn longest_palindrome(s: String) -> i64 {
                 t[i] += (j - i) as i64;
             }
         }
-        // for j in i..n {
-        //     // if
-        // }
     }
-// // for i in 0..n {
-//     // build subproblems
-//     for j in 0..n {
-//         println!("{}\n{}", fv_char(&v), fv_i64(&t));
-//         let c1 = v[j];
-//         let mut k = j + t[j] as usize;
-//         while k < n - 1 {
-//             k += t[k] as usize;
-//             let c2 = v[k];
-//             if c1 == c2 && (k - j) as i64 > t[j] {
-//                 t[j] = (k - j) as i64;
-//             }
-//         }
-//         // for k in j..n {
-//         //     let c2 = v[k];
-//         //     if (k - i) as i64 > t[i] && c1 == c2 {
-//         //         t[i] = (k - i) as i64;
-//         //         // this is almost it... just need to account for the fact that I am not checking if the middle is a palindrome
-//         //         // TODO: need to check each corresponding element and jump ahead that number
-//         //     }
-//         // }
-//     }
-// }
     // return max from list
     // code adapted from https://stackoverflow.com/questions/58669865/how-to-get-the-minimum-value-within-a-vector-in-rust
     let max_val = t.iter().max();
